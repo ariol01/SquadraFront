4031698b53e3a826f185365bbe94745413d33566
Ariel Veras <ariolvs123@gmail.com>
2022-06-28 13:36:52 -0400

    [tarefa] Adicionando pacotes [new] adicionado pacote e criado algumas estruturas de pasta

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Row, Col, Card, Nav } from 'react-bootstrap';
+import { CartaoLead } from './componentes/CartaoLead';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
 
 function App() {
+  
+  const [isActive, setActive] = useState(false);
+  
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <Card>
+          <Card.Header>
+            <Nav variant="pills" defaultActiveKey={isActive ? '#first': '"#link'}>
+              <Nav.Item>
+                <Nav.Link href="#first" onClick={toggleClass}>Invited</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#link" onClick={toggleClass}>Accepted</Nav.Link>
+              </Nav.Item>
+
+            </Nav>
+          </Card.Header>
+          <Card.Body>
+
+            <CartaoLead/>
+
+          </Card.Body>
+
+        </Card>      
+     
+      </>
     </div>
   );
 }
